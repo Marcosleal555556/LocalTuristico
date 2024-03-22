c96cb10570fa62bca635951f9a81d4d0f26af493
Marcosleal555556 <marcosantoniocordeiro27@gmail.com>
2024-03-22 08:24:17 -0300

    up

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import TouristLocation from "./src/components/TouristLocation";
 export default function App() {
   return (
     <View style={styles.container}>
-      <TouristLocation name="Neo Química Arena" />
-      <TouristLocation address="Av. Miguel Ignácio Curi, 111 - Vila Carmosina, São Paulo - SP, 08295-005" />
-      <TouristLocation imageUri='./src/Imagens/NeoQuimica.jpeg'>My App</TouristLocation>
-      <TouristLocation description="Descrição" />
+      <TouristLocation name="Neo Química Arena"
+      address="Av. Miguel Ignácio Curi, 111 - Vila Carmosina, São Paulo - SP, 08295-005"
+      NeoQuimica='./src/Imagens/NeoQuimica.jpeg'
+      description="Casa Do Sport Club Corinthians Paulista"
+      />
+   
     </View>
   );
 }
--- src/components/TouristLocation.js
+++ src/components/TouristLocation.js
@@ -5,11 +5,11 @@ const TouristLocation = (props) => {
   return (
     <View>
       <Text style={{ fontSize: 30 }}>{props.name}</Text>
-      <Text style={{ fontSize: 25, color: "darkgray" }}>{props.address}</Text>
-      <Image style={{height:50, width:50}} source={props.imageUri} />
+      <Text style={{ fontSize: 25, color: "#add8e6" }}>{props.address}</Text>
+      <Image style={{ height:500, width:500, alignItems:'left' }} source={props.NeoQuimica} />
         
 
-      <Text>{props.description}</Text>
+      <Text style={{ fontSize: 20, color: "#a9a9a9" }}>{props.description}</Text>
     </View>
   );
 };
